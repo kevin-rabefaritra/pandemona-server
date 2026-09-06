b6cd7413056fb7f9dd2d99851d76425ba954ddb5
Kevin Michel <kevin.rabefaritra@gmail.com>
2026-09-06 10:48:55 +0300

    fix: 1.0.5 updated version endpoints for retro-compatibility

--- src/main/java/studio/startapps/pandemona/version/mobile/VersionMobileControllerV1.java
+++ src/main/java/studio/startapps/pandemona/version/mobile/VersionMobileControllerV1.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 import studio.startapps.pandemona.version.internal.AppVersion;
 
 @RestController
-@RequestMapping(value = "/api/mobile/version", version = "2.0")
+@RequestMapping("/api/mobile/v1/version")
 @RequiredArgsConstructor
-public class VersionMobileController {
+@Deprecated
+public class VersionMobileControllerV1 {
 
     private final VersionMobileService versionMobileService;
 
